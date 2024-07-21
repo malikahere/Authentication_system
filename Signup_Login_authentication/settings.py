f92c02ea90d2c856068217ab241d8c0e0bce4289
Malika Garg <malikagarg121@gmail.com>
2024-07-22 01:14:10 +0530

    Settings updated

--- Signup_Login_authentication/settings.py
+++ Signup_Login_authentication/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
