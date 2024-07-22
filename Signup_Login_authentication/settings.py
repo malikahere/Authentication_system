bb7f5bf44d01ebf8c3c5aaca2c64ed8da01ba879
Malika Garg <malikagarg121@gmail.com>
2024-07-22 18:06:42 +0530

    updated

--- Signup_Login_authentication/settings.py
+++ Signup_Login_authentication/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['authentication-system-8021.onrender.com'] 
+ALLOWED_HOSTS = ['authentication-system-8021.onrender.com', '127.0.0.1'] 
 
 
 # Application definition
